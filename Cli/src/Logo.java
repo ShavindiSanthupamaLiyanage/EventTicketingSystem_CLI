ea3c23ee59c7b5cc1bc57d3a0f485ad6b7d01f41
ShavindiSanthupamaLiyanage <shavis1113@gmail.com>
2024-11-24 21:52:58 +0530

    commit

---
+++ Cli/src/Logo.java
@@ -0,0 +1,15 @@
+//public class Logo {
+//
+//    public static void printBoxedMessage(String message) {
+//        int messageLength = message.length();
+//        int boxWidth = messageLength + 4; // Add padding for the border
+//
+//        // Create the top and bottom border
+//        String border = "*".repeat(boxWidth);
+//
+//        // Print the box
+//        System.out.println(border);
+//        System.out.println("* " + message + " *");
+//        System.out.println(border);
+//    }
+//}
